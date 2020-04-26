76185197c0931be04e9c20177825e9ba18c8e4a7
xnocken <minecraftmarc746@gmail.com>
2020-04-26 18:52:36 +0200

    feat(features): adds new paths

--- discord.js
+++ discord.js
@@ -1,150 +1,173 @@
 const request = require('request');
 const WebSocket = require('ws');
-let key = '';
+const image2base64 = require('image-to-base64');
 
-function init(authKey = '') {
-    key = authKey;
-};
-
-function raw(content = '', url = '', method = 'post', callback = () => {}) {
-    sendRequest(content, url, method, callback)
+class Discord {
+  constructor(authKey) {
+    this.key = authKey;
+  }
 }
 
-function message(server = '', content = '', callback = () => {}) {
-    if (!server) {
-        console.error('Server is needed');
-        return;
-    }
+Discord.prototype.raw = function(content = '', url = '', method = 'post', callback = () => {}) {
+  this.sendRequest(content, url, method, callback)
+}
 
+Discord.prototype.guildInfo = function(guild_id = '', callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/guilds/${guild_id}`;
+
+  this.sendRequest('', url, 'GET', callback);
+}
+
+Discord.prototype.guildChannels = function(guild_id = '', callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/guilds/${guild_id}/channeös`;
+
+  this.sendRequest('', url, 'GET', callback);
+}
+
+Discord.prototype.changeHypesquad = function(squadid = 1, callback = () => {}) {
+  const url = "https://discordapp.com/api/v6/hypesquad/online";
+  const body = {
+    house_id: squadid,
+  }
+
+  this.sendRequest(JSON.stringify(body), url, 'POST', callback)
+}
+
+Discord.prototype.uploadEmoji = function(pathToEmoji = '', guild_id = '', emojiName = '', callback = function() {}) {
+  image2base64(pathToEmoji).then(function(response) {
+    const url = `https://discordapp.com/api/v6/guilds/${guild_id}/emojis`;
     const body = {
-        content: content,
-        nonce: String(Math.random()),
-        tts: 'false',
-    }
-    const url = `https://discordapp.com/api/v6/channels/${(server)}/messages`;
-
-    sendRequest(JSON.stringify(body), url, 'POST', callback)
-}
-
-function embed(server = '', embed = {}, callback = () => {}) {
-    if (!server) {
-        console.error('Server is needed');
-        return;
+      image: 'data:image/png;base64,' + response,
+      name: emojiName,
     }
 
-    const body = {
-        nonce: String(Math.random()),
-        tts: 'false',
-        embed,
-    }
-    const url = `https://discordapp.com/api/v6/channels/${(server)}/messages`;
-
-    sendRequest(JSON.stringify(body), url, 'POST', callback)
-}
-function deleteMessage(channelId = '', messageId = '', callback = () => {} ) {
-    const url = `https://discordapp.com/api/v6/channels/${channelId}/messages/${messageId}`;
-
-    sendRequest('', url, 'DELETE', callback);
+    this.sendRequest(JSON.stringify(body), url, 'POST', callback);
+  });
 }
 
-function getMessages(server = '', count = 0, callback = () => {}) {
-    const url = `https://discordapp.com/api/v6/channels/${server}/messages?limit=${count}`;
+Discord.prototype.message = function(server = '', content = '', callback = () => {}) {
+  if (!server) {
+    console.error('Server is needed');
+    return;
+  }
 
-    sendRequest('', url, 'GET', callback);
+  const body = {
+    content: content,
+    nonce: String(Math.random()),
+    tts: 'false',
+  }
+
+  const url = `https://discordapp.com/api/v6/channels/${server}/messages`;
+
+  this.sendRequest(JSON.stringify(body), url, 'POST', callback)
 }
 
-function typing(server = '') {
-    const url = `https://discordapp.com/api/v6/channels/${server}/typing`;
-    sendRequest('', url, 'POST')
-}
+Discord.prototype.embed = function(server = '', embed = {}, callback = () => {}) {
+  if (!server) {
+    console.error('Server is needed');
+    return;
+  }
 
-function invites(server = '', max_age = 0, max_uses = 0, temporary = false, callback = () => {}) {
-    const body = {
-        max_age,
-        max_uses,
-        temporary,
-    }
-    const url = `https://discordapp.com/api/v6/channels/${server}/invites`;
-
-    sendRequest(JSON.stringify(body), url, 'POST', callback);
-    return (handleReturn());
-}
-
-function science(token = '', events = {}) {
-    const body = {
-        events,
-        token,
-    }
-
-    sendRequest(JSON.stringify(body), 'https://discordapp.com/api/v6/science', 'POST');
-}
-
-function changeRole(server = '', user = '', roles = [], callback = () => {}) {
-    const url = `https://discordapp.com/api/v6/guilds/${server}/members/${user}`;
-    sendRequest(JSON.stringify({ roles }), url, 'PATCH', callback)
-}
-
-function checkInvite(code = '', callback = () => {}) {
-    const url = `https://discordapp.com/api/v6/invites/${code}`;
-    sendRequest('', url, 'POST', callback)
-}
-
-function getUserInfo(userId = '', callback = () => {}) {
-    const url = `https://discordapp.com/api/v6/users/${userId}`;
-
-    sendRequest('', url, 'GET', callback)
-}
-
-function redeemCode(code = '', callback = () => {}) {
-    const url = `https://discordapp.com/api/v6/entitlements/gift-codes/${code}/redeem`;
-
-    sendRequest('{"channel_id":null,"payment_source_id":null}', url, 'POST', callback)
-}
-
-function sendRequest(body = '', url = '', method = '', callback = () => {}) {
-    var settings = {
-        url,
-        method,
-        "headers": {
-            "authorization": key,
-            "Content-Type": "application/json",
-        },
-        body,
-    }
-
-    request(settings, function (err, res, body) {
-        callback(body);
-    });
-}
-
-
-function connectGateway(onopen = () => { }, plattform = 'Windows') {
-    const websocket = new WebSocket('wss://gateway.discord.gg/');
-    websocket.onopen = () => {
-        websocket.send(`{"op":2,"d":{"token":"${key}","properties":{"os":"${plattform}","browser":"Chrome","device":"","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36","browser_version":"81.0.4044.122","os_version":"10","referrer":"https://discord.app/","referring_domain":"discord.app","referrer_current":"https://discord.app/","referring_domain_current":"discord.app","release_channel":"stable","client_build_number":58583,"client_event_source":null},"presence":{"status":"online","since":0,"activities":[],"afk":true},"compress":false}}`);
-
-        setInterval(() => {
-            websocket.send('{"op":1,"d":638}');
-        }, 30000);
-        onopen();
-    };
-
-    return websocket;
-}
-
-module.exports = {
-    message,
-    typing,
-    init,
-    invites,
-    raw,
-    science,
-    changeRole,
-    checkInvite,
-    getMessages,
-    connectGateway,
+  const body = {
+    nonce: String(Math.random()),
+    tts: 'false',
     embed,
-    deleteMessage,
-    getUserInfo,
-    redeemCode,
-};
+  }
+  const url = `https://discordapp.com/api/v6/channels/${(server)}/messages`;
+
+  this.sendRequest(JSON.stringify(body), url, 'POST', callback)
+}
+
+Discord.prototype.deleteMessage = function(channelId = '', messageId = '', callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/channels/${channelId}/messages/${ messageId } `;
+
+  this.sendRequest('', url, 'DELETE', callback);
+}
+
+Discord.prototype.getMessages = function(server = '', count = 0, callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/channels/${server}/messages?limit=${count}`;
+
+  this.sendRequest('', url, 'GET', callback);
+}
+
+Discord.prototype.typing = function(server = '') {
+  const url = `https://discordapp.com/api/v6/channels/${server}/typing`;
+  this.sendRequest('', url, 'POST')
+}
+
+Discord.prototype.invites = function(server = '', max_age = 0, max_uses = 0, temporary = false, callback = () => {}) {
+  const body = {
+    max_age,
+    max_uses,
+    temporary,
+  }
+
+  const url = `https://discordapp.com/api/v6/channels/${server}/invites`;
+
+  this.sendRequest(JSON.stringify(body), url, 'POST', callback);
+}
+
+Discord.prototype.science = function(token = '', events = {}) {
+  const body = {
+    events,
+    token,
+  }
+
+  this.sendRequest(JSON.stringify(body), 'https://discordapp.com/api/v6/science', 'POST');
+}
+
+Discord.prototype.changeRole = function(server = '', user = '', roles =[], callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/guilds/${server}/members/${user}`;
+  this.sendRequest(JSON.stringify({ roles }), url, 'PATCH', callback)
+}
+
+Discord.prototype.  checkInvite = function(code = '', callback = () => { }) {
+  const url = `https://discordapp.com/api/v6/invites/${code}`;
+  this.sendRequest('', url, 'POST', callback)
+}
+
+Discord.prototype.getUserInfo = function(userId = '', callback = () => {}) {
+  const url = `https://discordapp.com/api/v6/users/${userId} `;
+
+  this.sendRequest('', url, 'GET', callback)
+}
+
+Discord.prototype.redeemCode = function(code = '', callback = () => { }) {
+  const url = `https://discordapp.com/api/v6/entitlements/gift-codes/${code}/redeem`;
+
+  this.sendRequest('{"channel_id":null,"payment_source_id":null}', url, 'POST', callback)
+}
+
+Discord.prototype.sendRequest = function(body = '', url = '', method = '', callback = () => {}) {
+  var settings = {
+    url,
+    method,
+    "headers": {
+      "authorization": this.key,
+      "Content-Type": "application/json",
+    },
+    body,
+  }
+
+  request(settings, function (err, res, body) {
+    callback(body);
+  });
+}
+
+
+Discord.prototype.connectGateway = function(onopen = () => {}, plattform = 'Windows') {
+  this.gateway = new WebSocket('wss://gateway.discord.gg/');
+
+  this.gateway.onopen = () => {
+    this.gateway.send(`{"op":2,"d":{"token":"${this.key} ","properties":{"os":"${plattform} ","browser":"Chrome","device":"","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36","browser_version":"81.0.4044.122","os_version":"10","referrer":"https://discord.app/","referring_domain":"discord.app","referrer_current":"https://discord.app/","referring_domain_current":"discord.app","release_channel":"stable","client_build_number":58583,"client_event_source":null},"presence":{"status":"online","since":0,"activities":[],"afk":true},"compress":false}}`);
+
+    setInterval(() => {
+      this.gateway.send('{"op":1,"d":638}');
+    }, 30000);
+    onopen();
+  };
+
+  return this.gateway;
+}
+
+module.exports = Discord;
